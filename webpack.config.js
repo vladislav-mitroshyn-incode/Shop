5cde4156a0c4e7c412c5c058332b2a3765d34e63
taras_l <t.lozovoy@humandone.com>
2022-06-04 13:23:29 +0300

    feat: add less

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  module: {
+    rules: [
+      // Whatever other rules you have here...
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]"
+            }
+          },
+          {
+            loader: "less-loader"
+          }
+        ]
+      }
+    ]
+  },
+};
