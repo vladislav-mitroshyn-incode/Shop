630fcc98357af14c0c7af1745f4b8c338fd23417
Max <markushitstep@gmail.com>
2022-06-15 12:25:20 +0300

    feat(PopularCategories): create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Cards from './components/Cards/Cards';
 import Header from './components/Header/Header';
 import NewArrivals from './components/NewArrivals/NewArrivals';
+import PopularCategories from './components/PopularCategories/PopularCategories';
 import SectionSlider from './components/SectionSlider/SectionSlider';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <SectionSlider />
         <Cards />
         <NewArrivals />
+        <PopularCategories />
       </main>
     </div>
   );
---
+++ src/components/PopularCategories/PopularCategories.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+import styles from './PopularCategories.module.scss';
+import Tops from '../../assets/categories/image.png';
+import tShirts from '../../assets/categories/image1.png';
+import Caps from '../../assets/categories/image2.png';
+import Sandals from '../../assets/categories/image3.png';
+import Jackets from '../../assets/categories/image4.png';
+import Coats from '../../assets/categories/image5.png';
+import PopularCategoriItem from '../PopularCategoriItem/PopularCategoriItem';
+
+const PopularCategories: FC = () => {
+  const categories = [
+    { img: <img src={Tops} alt="Tops" />, name: 'Tops' },
+    { img: <img src={tShirts} alt="T-shirts" />, name: 'T-shirts' },
+    { img: <img src={Caps} alt="Caps" />, name: 'Caps' },
+    { img: <img src={Sandals} alt="Sandals" />, name: 'Sandals' },
+    { img: <img src={Jackets} alt="Jackets" />, name: 'Jackets' },
+    { img: <img src={Coats} alt="Coats" />, name: 'Coats' },
+  ];
+
+  return (
+    <section className={styles.popularCategories}>
+      <div className={styles.popularCategories__container}>
+        <h1>Popular categories</h1>
+        <PopularCategoriItem categories={categories} />
+      </div>
+    </section>
+  );
+};
+
+export default PopularCategories;
