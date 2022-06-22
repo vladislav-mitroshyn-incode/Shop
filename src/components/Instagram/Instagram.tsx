a3b45c14e72a2196daeff7d681fb8015930b72e5
Max <markushitstep@gmail.com>
2022-06-22 11:42:34 +0300

    feat: add ContainerComponent

--- src/components/Instagram/Instagram.tsx
+++ src/components/Instagram/Instagram.tsx
@@ -4,6 +4,7 @@ import styles from './Instagram.module.scss';
 import image from '../../assets/instagram/image.png';
 import image2 from '../../assets/instagram/image2.png';
 import image3 from '../../assets/instagram/image3.png';
+import ContainerComponent from '../UI/ContainerComponent/ContainerComponent';
 
 const instagramImages = [
   { img: <img src={image} alt="" /> },
@@ -14,24 +15,26 @@ const instagramImages = [
 const Instagram: React.FC = () => {
   return (
     <section className={styles.instagram}>
-      <div className={styles.instagram__container}>
-        <div className={styles.instagram__info}>
-          <p>Follow us on Instagram</p>
-          <h1>
-            <a href="#">@createx_store</a>
-          </h1>
-          <div className={styles.instagram__btn}>
-            <ShopButton className={styles.btn__icon} typeButton="outlined">
-              Follow instagram
-            </ShopButton>
+      <ContainerComponent>
+        <div className={styles.instagram__body}>
+          <div className={styles.instagram__info}>
+            <p>Follow us on Instagram</p>
+            <h1>
+              <a href="#">@createx_store</a>
+            </h1>
+            <div className={styles.instagram__btn}>
+              <ShopButton className={styles.btn__icon} typeButton="outlined">
+                Follow instagram
+              </ShopButton>
+            </div>
+          </div>
+          <div className={styles.instagram__images}>
+            {instagramImages.map((instagramImage, index) => (
+              <div key={index}>{instagramImage.img}</div>
+            ))}
           </div>
         </div>
-        <div className={styles.instagram__images}>
-          {instagramImages.map((instagramImage, index) => (
-            <div key={index}>{instagramImage.img}</div>
-          ))}
-        </div>
-      </div>
+      </ContainerComponent>
     </section>
   );
 };
