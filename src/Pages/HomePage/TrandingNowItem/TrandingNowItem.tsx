9959274a633b744f97fff50fbc171c89a5861d28
Max <markushitstep@gmail.com>
2022-06-24 17:21:36 +0300

    refactor: move component in folder HomePage

---
+++ src/Pages/HomePage/TrandingNowItem/TrandingNowItem.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Slider from 'react-slick';
+import { ITrandingData } from '../../../types/types';
+import styles from './TrandingNowItem.module.scss';
+
+interface TrandingNowItemProps {
+  data: ITrandingData[];
+}
+
+interface SettingsProp {
+  infinite: boolean;
+  dots: boolean;
+  arrow: boolean;
+  speed: number;
+  slidesToShow: number;
+  slidesToScroll: number;
+}
+
+const TrandingNowItem: React.FC<TrandingNowItemProps> = ({ data }) => {
+  const settings: SettingsProp = {
+    infinite: false,
+    dots: false,
+    arrow: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
+  return (
+    <Slider className={styles.tranding__card} {...settings}>
+      {data.map((card, index) => (
+        <div key={index}>
+          <div className={styles.card__image}>
+            <img src={card.image} alt={card.title} />
+          </div>
+          <div className={styles.card__subtitle}>
+            <div className={styles.card__title}>{card.title}</div>
+            <div className={styles.card__price}>$ {card.price}</div>
+          </div>
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default TrandingNowItem;
