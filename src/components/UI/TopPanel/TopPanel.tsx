66abef5196a3fcf2fc94f16495c41e3840a73c4d
Max <markushitstep@gmail.com>
2022-07-05 15:41:43 +0300

    refactor(TopPanel): add prevPage currenPage for link

--- src/components/UI/TopPanel/TopPanel.tsx
+++ src/components/UI/TopPanel/TopPanel.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import ContainerComponent from '../ContainerComponent/ContainerComponent';
 import styles from './TopPanel.module.scss';
 
 interface ITopPanelProps {
-  text: string;
+  prevPage?: string;
+  prevPageLink?: string;
+  currentPage: string | undefined;
 }
 
-const TopPanel: React.FC<ITopPanelProps> = ({ text }) => {
+const TopPanel: React.FC<ITopPanelProps> = ({ prevPage, prevPageLink, currentPage }) => {
   return (
-    <div className={styles.TopPanel}>
+    <div className={styles.topPanel}>
       <ContainerComponent>
-        <div className={styles.fashionBlog__panel}>
-          <input type="button" />
-          <button disabled type="button">
-            {text}
-          </button>
+        <div className={styles.topPanel__body}>
+          <NavLink to="/">
+            <input type="button" />
+          </NavLink>
+          {prevPage ? (
+            <div className={styles.topPanel__pagesLink}>
+              <NavLink to={`${String(prevPageLink)}`}>
+                <button className={styles.pagesLink__active} type="button">
+                  {prevPage}
+                </button>
+              </NavLink>
+              <button disabled type="button">
+                {currentPage}
+              </button>
+            </div>
+          ) : (
+            <div className={styles.topPanel__pagesLink}>
+              <button disabled type="button">
+                {currentPage}
+              </button>
+            </div>
+          )}
         </div>
       </ContainerComponent>
     </div>
