6303b3d9216b996b5c65339065d58cd0dc7bd3ea
Max <markushitstep@gmail.com>
2022-07-05 15:46:01 +0300

    feat: add opportunity to get fashion blogs

--- src/assets/urls.ts
+++ src/assets/urls.ts
@@ -1,3 +1,4 @@
 export const productsUtl = 'http://localhost:3001/products';
 export const trandingsUtl = 'http://localhost:3001/trandingNow';
 export const categoriesUtl = 'http://localhost:3001/popularCategories';
+export const fashionBlogsUtl = 'http://localhost:3001/fashionBlogs';
