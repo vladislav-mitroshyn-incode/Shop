61e8c348bfcff375c4c97357a1a9704cf84dc8b8
Max <markushitstep@gmail.com>
2022-08-03 15:09:41 +0300

    feat: create transition on fashion blog

--- src/Pages/HomePage/FashionBlog/FashionBlog.tsx
+++ src/Pages/HomePage/FashionBlog/FashionBlog.tsx
@@ -1,34 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, NavLink } from 'react-router-dom';
 import styles from './FashionBlog.module.scss';
-import bag from '../../../assets/fashion-blog/bag.webp';
-import sneaker from '../../../assets/fashion-blog/sneaker.webp';
 import ShopButton from '../../../components/UI/ShopButton/ShopButton';
 import ContainerComponent from '../../../components/UI/ContainerComponent/ContainerComponent';
-
-const fashionBlogs = [
-  {
-    title: 'Bag Trends for Summer 2020',
-    type: 'Fashion',
-    data: { month: 'August', day: 24, year: 2020 },
-    comments: null,
-    subTitle:
-      'Ipsum aliquet nisi, hendrerit rhoncus quam tortor, maecenas faucibus. Tincidunt aliquet sit vel, venenatis nulla. Integer bibendum turpis convallis...',
-    img: <img src={bag} alt="bag" />,
-  },
-  {
-    title: 'Top 10 of This Season’s Hottest Sneakers',
-    type: 'Lifestyle',
-    data: { month: 'July', day: 16, year: 2020 },
-    comments: 4,
-    subTitle:
-      'Porta habitant vitae quam interdum. Leo viverra non volutpat rhoncus placerat vitae scelerisque. Rhoncus augue faucibus maecenas lacus...',
-    img: <img src={sneaker} alt="sneaker" />,
-  },
-];
+import { getFashionBlogsData } from '../../../services';
+import { IFashionBlogData } from '../../../types';
 
 const FashionBlog = () => {
   const navigate = useNavigate();
+  const [data, setData] = useState<IFashionBlogData[]>([]);
+
+  useEffect(() => {
+    getFashionBlogsData()
+      .then((response) => {
+        const currentIdBlogs = { firstId: 1, secondId: 2 };
+        setData(response.filter((blog) => blog.id === currentIdBlogs.firstId || blog.id === currentIdBlogs.secondId));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <section className={styles.fashionBlog}>
@@ -45,14 +36,18 @@ const FashionBlog = () => {
             </div>
           </div>
           <div className={styles.fashionBlog__content}>
-            {fashionBlogs.map((fashionBlog, index) => (
+            {data.map((fashionBlog, index) => (
               <div key={index} className={styles.fashionBlog__blog}>
-                <div className={styles.blog__image}>{fashionBlog.img}</div>
+                <div className={styles.blog__image}>
+                  <img src={fashionBlog.img} alt={fashionBlog.title} />
+                </div>
                 <div className={styles.blog__body}>
-                  <h2>{fashionBlog.title}</h2>
+                  <NavLink to={`blogs/${fashionBlog.id}`}>
+                    <h2>{fashionBlog.title}</h2>
+                  </NavLink>
                   <div className={styles.blog__info}>
                     <span>{fashionBlog.type}</span>
-                    <span>{`${fashionBlog.data.month} ${fashionBlog.data.day}, ${fashionBlog.data.year}`}</span>
+                    <span>{`${fashionBlog.date.month} ${fashionBlog.date.day}, ${fashionBlog.date.year}`}</span>
                     <span>{fashionBlog.comments === null ? 'No comments' : `${fashionBlog.comments} comments`}</span>
                   </div>
                   <p>{fashionBlog.subTitle}</p>
