5d8616f015c23759162bef410f7f0723059887c1
taras_l <t.lozovoy@humandone.com>
2022-08-23 18:48:22 +0300

    feat(Categories): add dynamically showing stars

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import c from 'classnames';
 import { ICategoriesData } from '../../../types';
 import styles from './Card.module.scss';
 
@@ -7,23 +8,31 @@ interface ICard {
   className?: string;
 }
 
+const MAX_AMOUNT_STARS = 5;
+
+const showStars = (amountStars: number): JSX.Element[] => {
+  const starsElements = [];
+
+  for (let i = 0; i < MAX_AMOUNT_STARS; i++) {
+    const elem = (
+      <div
+        className={c(styles.star, { [styles.activeStar]: i < amountStars || amountStars === MAX_AMOUNT_STARS })}
+        style={{ left: `${200 + i * 14}px` }}
+      />
+    );
+    starsElements.push(elem);
+  }
+  return starsElements;
+};
+
 const Card: React.FC<ICard> = ({ cardData, className }) => {
   return (
     <div className={className}>
       <div className={styles.card__content}>
-        {cardData.stars === null ? (
-          <div className={styles.card__image}>
-            <img src={cardData.image} alt={cardData.title} />
-          </div>
-        ) : cardData.stars === 5 ? (
-          <div className={styles.card__image_five}>
-            <img src={cardData.image} alt={cardData.title} />
-          </div>
-        ) : (
-          <div className={styles.card__image_four}>
-            <img src={cardData.image} alt={cardData.title} />
-          </div>
-        )}
+        <div className={styles.card__image}>
+          <div className={styles.stars}>{cardData?.stars !== null ? showStars(cardData?.stars) : null}</div>
+          <img src={cardData.image} alt={cardData.title} />
+        </div>
         <div className={styles.card__subtitle}>
           <div className={styles.card__title}>{cardData.title}</div>
           <div className={styles.card__price}>$ {cardData.price}</div>
