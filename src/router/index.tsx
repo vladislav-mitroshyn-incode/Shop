a8dded2317e711106c3b13fdc1540ec1f7f72977
Maxim <markushitstep@gmail.com>
2022-08-23 15:21:53 +0300

    feat(PopularCategoriItem): add a redirect by clicking

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,3 +1,4 @@
+import CategoriesPage from '../Pages/CategoriesPage/CategoriesPage';
 import FashionBlogItemPage from '../Pages/FashionBlogPage/FashionBlogItemPage/FashionBlogItemPage';
 import FashionBlogPage from '../Pages/FashionBlogPage/FashionBlogPage';
 import HomePage from '../Pages/HomePage/HomePage';
@@ -6,4 +7,5 @@ export const routes = [
   { path: '/', component: <HomePage /> },
   { path: '/blogs', component: <FashionBlogPage /> },
   { path: '/blogs/:id', component: <FashionBlogItemPage /> },
+  { path: '/categorie/:name', component: <CategoriesPage /> },
 ];
