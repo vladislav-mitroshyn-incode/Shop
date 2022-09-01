0f8577a9447f2f919b7c48f77da63f3072cc74e5
Maxim <markushitstep@gmail.com>
2022-09-01 12:00:48 +0300

    refactor(Card): rename classes names

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import c from 'classnames';
-import { ICategoriesData } from '../../../types';
 import styles from './Card.module.scss';
+import { ICategoriesData } from '../../../types';
 
 interface ICard {
   cardData: ICategoriesData;
   className?: string;
 }
 
-const MAX_AMOUNT_STARS = 5;
-
-const showStars = (amountStars: number): JSX.Element[] => {
-  const starsElements = [];
-
-  for (let i = 0; i < MAX_AMOUNT_STARS; i++) {
-    const elem = (
-      <div
-        className={c(styles.star, { [styles.activeStar]: i < amountStars || amountStars === MAX_AMOUNT_STARS })}
-        style={{ left: `${200 + i * 14}px` }}
-      />
-    );
-    starsElements.push(elem);
-  }
-  return starsElements;
-};
-
 const Card: React.FC<ICard> = ({ cardData, className }) => {
+  const MAX_AMOUNT_STARS = 5;
+
+  const showStars = (amountStars: number): JSX.Element[] => {
+    const starsElements = [];
+
+    for (let i = 0; i < MAX_AMOUNT_STARS; i++) {
+      const elem1 = (
+        <div
+          key={i}
+          className={c(styles.card__star, { [styles.activeStar]: i < amountStars || amountStars === MAX_AMOUNT_STARS })}
+          style={{ left: `${200 + i * 14}px` }}
+        />
+      );
+      starsElements.push(elem1);
+    }
+    return starsElements;
+  };
+
   return (
     <div className={className}>
       <div className={styles.card__content}>
         <div className={styles.card__image}>
-          <div className={styles.stars}>{cardData?.stars !== null ? showStars(cardData?.stars) : null}</div>
+          <div className={styles.card__stars}>{cardData?.stars !== null ? showStars(cardData?.stars) : null}</div>
           <img src={cardData.image} alt={cardData.title} />
         </div>
         <div className={styles.card__subtitle}>
